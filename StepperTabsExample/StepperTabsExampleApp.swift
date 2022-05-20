cdf1042c5846bc8fae6ed4670bf7a3160fe6f5b2
Sebastián García Burgos <sebas.garciaburgos@gmail.com>
2022-05-20 10:28:51 +0200

    Initial project set up

---
+++ StepperTabsExample/StepperTabsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperTabsExampleApp.swift
+//  StepperTabsExample
+//
+//  Created by Sebastián García Burgos on 20/5/22.
+//
+
+import SwiftUI
+
+@main
+struct StepperTabsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
