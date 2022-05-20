b34a0f77ad9f7b71150ad91cb9a3526071332ad8
Sebastián García Burgos <sebas.garciaburgos@gmail.com>
2022-05-20 10:17:59 +0200

    Initial Commit

---
+++ StepperTabs/StepperTabs.h
@@ -0,0 +1,18 @@
+//
+//  StepperTabs.h
+//  StepperTabs
+//
+//  Created by Sebastián García Burgos on 20/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StepperTabs.
+FOUNDATION_EXPORT double StepperTabsVersionNumber;
+
+//! Project version string for StepperTabs.
+FOUNDATION_EXPORT const unsigned char StepperTabsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StepperTabs/PublicHeader.h>
+
+
